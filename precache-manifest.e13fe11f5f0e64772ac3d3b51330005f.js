0fa20c260ccb9cb7df6fac4e991f699a69087486
Andrey <a.kovalenok679@gmail.com>
2020-10-25 15:39:56 +0200

    Updates

---
+++ precache-manifest.e13fe11f5f0e64772ac3d3b51330005f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "588c4aa4d1393cb29ecf796f292f69ea",
+    "url": "/kode-test/index.html"
+  },
+  {
+    "revision": "6e6da385569fb3693b46",
+    "url": "/kode-test/static/css/main.438bd615.chunk.css"
+  },
+  {
+    "revision": "2ad44ed714a75df3e558",
+    "url": "/kode-test/static/js/2.b5e55913.chunk.js"
+  },
+  {
+    "revision": "cebb3a342e4431e19beec7ecc4a026b7",
+    "url": "/kode-test/static/js/2.b5e55913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e6da385569fb3693b46",
+    "url": "/kode-test/static/js/main.8d6dc296.chunk.js"
+  },
+  {
+    "revision": "69596608abf1cb3f4944",
+    "url": "/kode-test/static/js/runtime-main.803cb6b6.js"
+  },
+  {
+    "revision": "58bf8e0bd1dd213bf745e9d748caf175",
+    "url": "/kode-test/static/media/Roboto.58bf8e0b.woff"
+  },
+  {
+    "revision": "69eee0b43d46be3ad91ef75a76ea3959",
+    "url": "/kode-test/static/media/Roboto.69eee0b4.woff2"
+  },
+  {
+    "revision": "2090aeccce713476ee4fde0f300c1951",
+    "url": "/kode-test/static/media/arrow.2090aecc.svg"
+  },
+  {
+    "revision": "8454dfc3841ddad878e1d8c258a7e328",
+    "url": "/kode-test/static/media/dropdown-arrow.8454dfc3.svg"
+  }
+]);
